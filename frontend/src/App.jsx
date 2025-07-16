b739b05fefe132e2588911e357bd048d1d7a0c0f
Dara Soares <128198964+soaresdararm@users.noreply.github.com>
2025-07-16 00:30:00 -0300

    feat: update frontend dependencies and add Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-  return <div>APP</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div className="container">
+          {/* Logo and Links section */}
+          <div>
+            <a href="#" className=" text-primary">
+              Eshop
+            </a>
+          </div>
+
+          {/* Navbar Right section */}
+        </div>
+      </div>
+    </div>
+  );
+};
